4604e4e100cf68acf56939c68530868c658d3b2f
Matheus Paleari <mathe230@gmail.com>
2024-09-02 12:50:19 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Inicializa as variáveis
+indice = 12
+soma = 0
+k = 1
+
+# Loop enquanto k for menor que indice
+while k < indice:
+    k = k + 1
+    soma = soma + k
+
+# Imprime o resultado da soma
+print(soma)
